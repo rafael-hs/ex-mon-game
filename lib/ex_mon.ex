e6fc0cf223fac4a3d90297677f9d9fa738084c56
Rafael Honório <rafael.contratotrab@gmail.comcd>
2021-01-19 15:51:43 -0300

    implementing game status

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,7 +1,18 @@
 defmodule ExMon do
-  alias ExMon.Player
+  alias ExMon.{Game, Player}
+  alias ExMon.Game.Status
+
+  @robot_name "Robotinik"
 
   def create_player(name, move_avg, move_heal, move_rnd) do
     Player.build(name, move_avg, move_heal, move_rnd)
+  end
+
+  def start_game(player) do
+    @robot_name
+    |> create_player(:punch, :kick, :heal)
+    |> Game.start(player)
+
+    Status.print_round_message()
   end
 end
